03f447a24bc7305238d234c8cf9f7d70e07fe090
Zhizhou Qiu <qzhizhou@bu.edu>
2019-03-25 20:03:30 -0400

    update script file

--- web-crawler/start-my-crawler.sh
+++ web-crawler/start-my-crawler.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -jar target/my-crawler-1.0.0.BUILD-SNAPSHOT.jar rawQuerySmall.txt adsDataFile proxylist_bittiger.csv logFile
+java -jar target/my-crawler-1.0.0.BUILD-SNAPSHOT.jar rawQuery.txt adsDataFile proxylist.csv logFile
